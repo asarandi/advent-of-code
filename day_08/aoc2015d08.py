31527e7db7b1b35328900bde0edfa77b8a10040b
Alexandr SARANDI <sarandi.alex@gmail.com>
2020-02-19 19:09:22 -0800

    day_08

---
+++ day_08/aoc2015d08.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+with open("input.txt", "r") as fp:
+    data = fp.read().splitlines()
+    fp.close()
+
+a = b = c = 0
+for line in data:
+    a += len(line)
+    b += len(eval(line))
+    c += len(line.encode('unicode_escape').replace(b'"', b'\\"')) + 2
+
+print("aoc2015d08p01:",a-b)
+print("aoc2015d08p02:",c-a)
