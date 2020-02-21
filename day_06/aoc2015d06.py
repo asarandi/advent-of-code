242d069452c7667e9e39346c3c510ecc004ab76e
Alexandr SARANDI <sarandi.alex@gmail.com>
2020-02-20 22:54:03 -0800

    day 06 cosmetics

--- day_06/aoc2015d06.py
+++ day_06/aoc2015d06.py
@@ -21,13 +21,8 @@ for line in data:
     for i in range(int(src[0]), int(dst[0]) + 1):        
         for j in range(int(src[1]), int(dst[1]) + 1):
             g1[i][j] = g1[i][j] ^ 1 if op == 2 else op
-            if op == 0:
-                g2[i][j] = 0 if g2[i][j] < 1 else g2[i][j] - 1
+            g2[i][j] = 0 if g2[i][j] < 1 else g2[i][j] - 1 if op == 0 else g2[i][j]
             g2[i][j] += op
 
-p1 = p2 = 0
-for i in range(1000):
-    p1 += sum(g1[i])
-    p2 += sum(g2[i])
-print("aoc2015d06p1:", p1)
-print("aoc2015d06p2:", p2)
+print("aoc2015d06p01:", sum([sum(x) for x in g1]))
+print("aoc2015d06p02:", sum([sum(x) for x in g2]))
