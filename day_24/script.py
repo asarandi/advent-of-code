a2b66faf08b6d25143b279645dd89689f58d301a
Alexandr SARANDI <sarandi.alex@gmail.com>
2021-08-09 22:45:18 -0700

    day 24: faster

--- day_24/script.py
+++ day_24/script.py
@@ -4,38 +4,27 @@ import functools
 
 
 def search(arr: [int], n: int, s: int) -> [tuple]:
-    res = set()
-    used = [False for _ in range(len(arr))]
+    res = []
 
-    def dfs(tab: [int] = []) -> None:
+    def dfs(tab: [int] = [], i: int = -1) -> None:
         if len(tab) == n:
             if sum(tab) == s:
-                res.add(tuple(sorted(tab)))
+                res.append(tuple(tab))
         else:
-            for i in range(len(arr)):
-                if used[i] == False:
-                    used[i] = True
-                    dfs(tab + [arr[i]])
-                    used[i] = False
+            for j in range(i + 1, len(arr)):
+                dfs(tab + [arr[j]], j)
 
     dfs()
-    return list(res)
-
-
-def min_product(arr: [int]) -> int:
-    res = functools.reduce(lambda x, y: x * y, list(arr[0]))
-    for t in arr:
-        p = functools.reduce(lambda x, y: x * y, list(t))
-        res = p if p < res else res
     return res
 
 
-N = 2; M = 2; nums = [1, 2, 3, 4, 5, 7, 8, 9, 10, 11]
+def min_product(arr: [tuple]) -> int:
+    return min(map(lambda t: functools.reduce(lambda x, y: x * y, t), arr))
 
-#N = 6; M = 5; nums = [1,3,5,11,13,17,19,23,29,31,37,41,43,47,53,59,67,71,73,79,83,89,97,101,103,107,109,113]
 
-res = search(nums, N, sum(nums) // 3)
-print("part 1:", min_product(res))
+with open("input.txt") as fp:
+    nums = [int(i) for i in fp.read().splitlines()]
+    fp.close()
 
-res = search(nums, M, sum(nums) // 4)
-print("part 2:", min_product(res))
+print("part 1:", min_product(search(nums, 6, sum(nums) // 3)))
+print("part 2:", min_product(search(nums, 5, sum(nums) // 4)))
