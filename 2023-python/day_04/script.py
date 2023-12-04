6b6ebc70df07a7f7c9adb492e50b7ae06fdea19d
Alexandr SARANDI <sarandi.alex@gmail.com>
2023-12-04 05:35:58 -0800

    2023, day 4, faster

--- 2023-python/day_04/script.py
+++ 2023-python/day_04/script.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 
-p1, p2 = 0, 0
 data = open("input.txt").read().splitlines()
-rep = [0] * len(data)
+p1, p2 = 0, [0] * len(data)
 for i, s in enumerate(data):
     l, r = map(str.split, s.split(" | "))
     n = len(set(l) & set(r))
-    for _ in range(rep[i] + 1):
-        for j in range(i + 1, n + i + 1):
-            rep[j] += 1
     p1 += 2 ** (n - 1) if n else 0
-    p2 += rep[i] + 1
-print(p1, p2)
+    p2[i] += 1
+    for j in range(i + 1, n + i + 1):
+        p2[j] += p2[i]
+print(p1, sum(p2))
