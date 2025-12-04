b545fd8b00ddf8e55bba9ed286f4967bc7bbf04b
Alexandr SARANDI <sarandi.alex@gmail.com>
2025-12-04 02:36:16 -0500

    2025, day 4, use sets

--- 2025-python/day_04/script.py
+++ 2025-python/day_04/script.py
@@ -1,35 +1,28 @@
 #!/usr/bin/env python3
 
 
-def get_input(filename: str) -> dict:
+def get_input(filename: str) -> set:
     lines = open(filename).read().splitlines()
     h, w = len(lines), len(lines[0])
-    return {(y, x): True for y in range(h) for x in range(w) if lines[y][x] == "@"}
+    return {(y, x) for y in range(h) for x in range(w) if lines[y][x] == "@"}
 
 
-def count_adjacent(grid: dict, yx: tuple) -> int:
-    adjacent = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-    add_tuples = lambda a, b: tuple(map(sum, zip(a, b)))
-    return sum([1 if add_tuples(adj, yx) in grid else 0 for adj in adjacent])
+def get_adjacent(grid: set, yx: tuple) -> set:
+    adjacent = {(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)}
+    adjacent = set(map(lambda a: tuple(map(sum, zip(a, yx))), adjacent))
+    return grid & adjacent
 
 
-def simulate_updates(grid: dict, ct: int = 0) -> int:
-    grid_copy = grid.copy()
-
-    ct_prev = ct
-    for key in grid.keys():
-        if count_adjacent(grid, key) < 4:
-            del grid_copy[key]
-            ct += 1
-
-    return ct if ct == ct_prev else simulate_updates(grid_copy, ct)
+def simulate_updates(grid: set) -> set:
+    removed = {t for t in grid if len(get_adjacent(grid, t)) < 4}
+    return grid if not removed else simulate_updates(grid - removed)
 
 
 def main():
     grid = get_input("input")
 
-    part_1 = sum([1 if count_adjacent(grid, key) < 4 else 0 for key in grid.keys()])
-    part_2 = simulate_updates(grid)
+    part_1 = sum([1 if len(get_adjacent(grid, t)) < 4 else 0 for t in grid])
+    part_2 = len(grid - simulate_updates(grid))
 
     print(part_1, part_2)
 
